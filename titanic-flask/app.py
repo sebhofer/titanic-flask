4c5cbe7f2502f12bd196040325bbbe6fa1e771c4
Sebastian Hofer <sebhofer@gmail.com>
2021-05-30 15:41:32 +0200

    add dummy prediction

--- titanic-flask/app.py
+++ titanic-flask/app.py
@@ -1,31 +1,19 @@
 import os
 
-from flask import Flask
+from flask import (
+    Flask, flash, g, redirect, render_template, request, session, url_for
+)
 
+app = Flask(__name__, template_folder="templates")
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-    )
+@app.route('/')
+def home():
+    return render_template("predict.html", prediction_text = "")
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+@app.route('/predict', methods=('GET', 'POST'))
+def predict():
+    if request.method == 'POST':
+        # do validation here
+        return render_template("predict.html", prediction_text = request.form['prediction'])
     else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    return app
+        return home()
